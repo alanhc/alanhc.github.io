ff6e125242dcb76afcbd8f1384836232ed679b0c
alanhc <alanhc.tseng1999@gmail.com>
2020-01-02 01:59:36 +0800

    111

---
+++ test/connectBLE.js
@@ -0,0 +1,41 @@
+
+let serviceUuid;
+let myCharacteristic;
+let myValue;
+let myBLE
+function initBLE() {
+  serviceUuid = "19B10010-E8F2-537E-4F6C-D104768A1214";
+  
+  myValue = 0;
+  
+  // Create a p5ble class
+  myBLE = new p5ble();
+
+
+
+  // Create a 'Connect' button
+  const connectButton = createButton('Connect')
+  connectButton.mousePressed(connectToBle);
+}
+function connectToBle() {
+    // Connect to a device by passing the service UUID
+    myBLE.connect(serviceUuid, gotCharacteristics);
+}
+// A function that will be called once got characteristics
+function gotCharacteristics(error, characteristics) {
+    if (error) console.log('error: ', error);
+    console.log('characteristics: ', characteristics);
+    myCharacteristic = characteristics[0];
+    // Read the value of the first characteristic
+    myBLE.read(myCharacteristic, gotValue);
+}
+
+  // A function that will be called once got values
+function gotValue(error, value) {
+    if (error) console.log('error: ', error);
+    console.log('value: ', value);
+    myValue = value;
+    // After getting a value, call p5ble.read() again to get the value again
+    myBLE.read(myCharacteristic, gotValue);
+    console.log(myValue);
+}
---
+++ test/sketch.js
@@ -0,0 +1,145 @@
+let socket;
+
+let players;
+let host;
+let myId;
+let serverIp='192.168.1.109:3000';
+let state;
+function setup() {
+    initBLE();
+
+    
+    createCanvas(windowWidth,windowHeight);
+    background(220);
+    socket = io('http://'+serverIp); //change server ip here
+    socket.on('players', (num)=>{  players=num; });
+    socket.on('hoster name', (hostInput)=>{  
+        host=hostInput;
+        if (host==myId) {
+            socket.emit('send pattern', myPattern);
+        }
+    });
+    socket.on('answer pattern', (patternIn)=>{
+        ansPattern=patternIn;
+    });
+    let init_myPattern=false;
+    let init_ansPattern=false;
+    
+    socket.on('beat', (stepsIn)=>{
+        havePlayed=false;
+        playBeat(steps, ansPattern);
+        steps=stepsIn;
+       
+        if (host!=myId) { // now is play mode
+            state='play';
+            
+            myPattern= [
+                [0,0,0,0,0,0,0,0], //bd
+                [0,0,0,0,0,0,0,0], //cr
+                [0,0,0,0,0,0,0,0], //hh
+                [0,0,0,0,0,0,0,0], //t1
+                [0,0,0,0,0,0,0,0], //t2
+            ]
+            init_myPattern=false;
+            
+            
+        } else {         // now is host mode
+            state='host';
+            playBeat(steps, myPattern);
+            ansPattern= [ 
+                [0,0,0,0,0,0,0,0], //bd
+                [0,0,0,0,0,0,0,0], //cr
+                [0,0,0,0,0,0,0,0], //hh
+                [0,0,0,0,0,0,0,0], //t1
+                [0,0,0,0,0,0,0,0], //t2
+            ]
+            if (!init_myPattern) {
+                myPattern= [
+                    [0,0,0,0,0,0,0,0], //bd
+                    [0,0,0,0,0,0,0,0], //cr
+                    [0,0,0,0,0,0,0,0], //hh
+                    [0,0,0,0,0,0,0,0], //t1
+                    [0,0,0,0,0,0,0,0], //t2
+                ]
+                init_myPattern=true;
+            }
+            
+            
+        }
+        //console.log(state);
+        
+    });
+}
+
+
+
+let steps;
+let havePlayed;
+function draw() {
+    myId=socket.id;
+    
+    if (key>0 && key<6 && keyIsPressed && state=='host') {
+        myPattern[key-1][steps]=1;
+        if (!havePlayed) {
+            playBeat(steps, myPattern);
+            havePlayed=true;
+        }
+    }
+    if (state=='host') background(255,0,0,10);
+    else background(0,128,220);
+    let spaceW=windowWidth/9;
+    let spaceH=windowHeight/7;
+    let shW=spaceW/2;
+    let shH=spaceH;
+
+    fill(0);
+    textSize(16);
+    text('online: '+players, shW, shH/3);
+    text('Host  : '+host, shW, shH/2);
+    text('state: '+ state, shW, shH/1.5);
+    
+    
+    for (let i=0; i<8; i++) {
+        for (let j=0; j<5; j++) {
+            if (myPattern[j][i]==1) {
+                
+                let minW=Math.min(spaceH,spaceW);
+                switch(j) {
+                    default:
+                    break; case 0: 
+                    image(img_bdrum, shW+spaceW*i, shH+spaceH*j, minW,minW);
+                    break; case 1:case 2:
+                    image(img_cymbals, shW+spaceW*i, shH+spaceH*j, minW,minW);
+                    break; case 3: case 4:
+                    image(img_drum, shW+spaceW*i, shH+spaceH*j, minW,minW);
+                }
+                
+                fill(0,30);
+            } else if (ansPattern[j][i]==1) {
+                
+                fill(150);
+                let minW=Math.min(spaceH,spaceW);
+                switch(j) {
+                    default:
+                    break; case 0: 
+                    image(img_bdrum, shW+spaceW*i, shH+spaceH*j, minW,minW);
+                    break; case 1:case 2:
+                    image(img_cymbals, shW+spaceW*i, shH+spaceH*j, minW,minW);
+                    break; case 3: case 4:
+                    image(img_drum, shW+spaceW*i, shH+spaceH*j, minW,minW);
+                }
+                
+            }
+            else {
+                fill(255);
+            }
+            //rect(shW+spaceW*i, shH+spaceH*j, spaceW,spaceH); //draw player
+            
+
+        }
+        fill(255,255,0,10);
+        rect(shW+steps*spaceW,shH,spaceW,spaceH*5);
+        stroke(0);
+    }
+}
+
